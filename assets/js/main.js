1b3a70d3c0282ab6e7cd38d4dcbf9f450dea10bb
faisal <faisaldevbhaluka@gmail.com>
2024-06-03 23:23:53 +0600

    now design complite all section html project

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,39 @@
          
       });
 
+        $(document).ready(function(){
+          $('.box-slider-active').slick({
+            autoplay:false,
+            slidesToShow: 4,
+            loop:true,
+            dots:false,
+            arrows:false,
+            responsive: [
+              {
+                breakpoint: 1199,
+                settings: {
+                  slidesToShow: 2,
+                }
+              },
+              {
+                breakpoint: 991,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 767,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
+         
+      });
+
 // WOW active
 new WOW().init();
 
